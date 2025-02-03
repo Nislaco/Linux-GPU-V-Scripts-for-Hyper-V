d017fb5362128e2441f0f09102edd1df94a46596
Nislaco <40778075+Nislaco@users.noreply.github.com>
2025-02-03 17:18:55 -0500

    Update (Ubuntu)24-build.sh
    Updated to work with newer kernels, up to 6.9.x.

--- (Ubuntu)24-build.sh
+++ (Ubuntu)24-build.sh
@@ -1,4 +1,4 @@
- #!/bin/bash -e
+#!/bin/bash -e
 BRANCH=linux-msft-wsl-6.6.y
 
 if [ "$EUID" -ne 0 ]; then
@@ -6,11 +6,12 @@ if [ "$EUID" -ne 0 ]; then
     exec sudo $0 "$@"
 fi
 
-apt-get install -y git dkms
+apt-get install -y git dkms curl dwarves
 
+cd /tmp
 git clone -b $BRANCH  --no-checkout  --depth=1 https://github.com/microsoft/WSL2-Linux-Kernel.git
 cd WSL2-Linux-Kernel
-git sparse-checkout set --no-cone /drivers/hv/dxgkrnl /include/uapi/misc/d3dkmthk.h /include/linux/hyperv.h
+git sparse-checkout set --no-cone /drivers/hv/dxgkrnl /include/uapi/misc/d3dkmthk.h /include/linux/hyperv.h /include/linux/eventfd.h  /include/linux/vmalloc.h /mm/vmalloc.c  /include/linux/kernel.h
 git checkout
 
 BATCH='test'
@@ -20,11 +21,18 @@ VERSION="${RUN}${BATCH}"
 cp -r drivers/hv/dxgkrnl /usr/src/dxgkrnl-$VERSION
 mkdir -p /usr/src/dxgkrnl-$VERSION/include/uapi/misc
 mkdir -p /usr/src/dxgkrnl-$VERSION/include/linux
+mkdir -p /usr/src/dxgkrnl-$VERSION/mm
 cp include/uapi/misc/d3dkmthk.h /usr/src/dxgkrnl-$VERSION/include/uapi/misc/d3dkmthk.h
 cp include/linux/hyperv.h /usr/src/dxgkrnl-$VERSION/include/linux/hyperv_dxgkrnl.h
+cp include/linux/eventfd.h /usr/src/dxgkrnl-$VERSION/include/linux/eventfd.h
+cp include/linux/vmalloc.h /usr/src/dxgkrnl-$VERSION/include/linux/vmalloc.h
+cp mm/vmalloc.c /usr/src/dxgkrnl-$VERSION/mm/vmalloc.c
+cp include/linux/kernel.h /usr/src/dxgkrnl-$VERSION/include/linux/kernel.h
 sed -i 's/\$(CONFIG_DXGKRNL)/m/' /usr/src/dxgkrnl-$VERSION/Makefile
+sed -i 's#<uapi/linux/eventfd.h>#<linux/eventfd.h>#g' /usr/src/dxgkrnl-$VERSION/include/linux/eventfd.h
 sed -i 's#linux/hyperv.h#linux/hyperv_dxgkrnl.h#' /usr/src/dxgkrnl-$VERSION/dxgmodule.c
-echo "EXTRA_CFLAGS=-I\$(PWD)/include -D_MAIN_KERNEL_" >> /usr/src/dxgkrnl-$VERSION/Makefile
+sed -i 's/l(event->cpu_event, 1)/l(event->cpu_event)/g' /usr/src/dxgkrnl-$VERSION/dxgmodule.c
+echo "EXTRA_CFLAGS=-I\$(PWD)/include -D_MAIN_KERNEL_ -DCONFIG_DXGKRNL=m -Wno-empty-body -Wno-int-conversion -Wno-implicit-function-declaration -Wno-int-to-pointer-cast -Wno-implicit -include /usr/src/dxgkrnl-$VERSION/include/extra-defines.h  -include /usr/src/dxgkrnl-$VERSION/include/linux/vmalloc.h  -include /usr/src/dxgkrnl-$VERSION/include/linux/kernel.h " >> /usr/src/dxgkrnl-$VERSION/Makefile
 
 cat > /usr/src/dxgkrnl-$VERSION/dkms.conf <<EOF
 PACKAGE_NAME="dxgkrnl"
@@ -34,10 +42,10 @@ DEST_MODULE_LOCATION="/kernel/drivers/hv/dxgkrnl/"
 AUTOINSTALL="yes"
 EOF
 
-wget https://raw.githubusercontent.com/thexperiments/dxgkrnl-dkms-git/main/dxgkrnl.h.patch
-patch -u /usr/src/dxgkrnl-$VERSION/dxgkrnl.h ./dxgkrnl.h.patch
-sed -i 's/l(event->cpu_event, 1)/l(event->cpu_event)/g' /usr/src/dxgkrnl-$VERSION/dxgmodule.c
-
+wget https://raw.githubusercontent.com/MBRjun/dxgkrnl-dkms-lts/master/extra-defines.h
+cp extra-defines.h  /usr/src/dxgkrnl-$VERSION/include/extra-defines.h
+# if BTF fails or you need vmlinux
+cp /sys/kernel/btf/vmlinux /usr/lib/modules/`uname -r`/build/
 
 dkms add dxgkrnl/$VERSION
 dkms build dxgkrnl/$VERSION
